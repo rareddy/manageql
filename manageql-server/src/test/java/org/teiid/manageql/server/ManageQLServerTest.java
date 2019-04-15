5efe6f4d3975d2d9a24aeabd25ecde05ee0372b7
Hiram Chirino <hiram@hiramchirino.com>
2019-04-15 11:08:50 -0400

    Simplify JMX tabular type json encoding, update to latest jdbi, and imx complex type tests. (#3)

--- manageql-server/src/test/java/org/teiid/manageql/server/ManageQLServerTest.java
+++ manageql-server/src/test/java/org/teiid/manageql/server/ManageQLServerTest.java
@@ -22,34 +22,40 @@ import static org.junit.Assert.assertTrue;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
+import org.jdbi.v3.core.ConnectionFactory;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.result.ResultIterable;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.skife.jdbi.v2.DBI;
-import org.skife.jdbi.v2.NoOpStatementRewriter;
-import org.skife.jdbi.v2.tweak.ConnectionFactory;
+
+import com.google.gson.Gson;
 
 /**
  * Unit test for simple App.
  */
 public class ManageQLServerTest {
     ManageQLServer server;
-    DBI dbi;
+    Jdbi dbi;
 
     @Before
     public void before() {
         server = new ManageQLServer();
         server.setPsqlPortNumber(0);
         server.start();
-        dbi = new DBI(new ConnectionFactory() {
+        dbi = Jdbi.create(new ConnectionFactory() {
             @Override
             public Connection openConnection() throws SQLException {
                 return server.getDriver().connect("jdbc:teiid:manageql", null);
             }
         });
-        dbi.setStatementRewriter(new NoOpStatementRewriter());
+        // dbi.setStatementRewriter(new NoOpStatementRewriter());
     }
 
     @After
@@ -61,14 +67,14 @@ public class ManageQLServerTest {
     @Test
     public void canSelectFromRuntimeMBean() throws SQLException {
         String specName = dbi.withHandle(db ->
-                db.createQuery("SELECT SpecName FROM \"jmx.java.lang:type=Runtime\"").mapTo(String.class).first());
+                db.createQuery("SELECT SpecName FROM \"jmx.java.lang:type=Runtime\"").mapTo(String.class).findOnly());
         assertEquals("Java Virtual Machine Specification", specName);
     }
 
     @Test
     public void canSelectObjectNameFromRuntimeMBean() throws SQLException {
         String objectName = dbi.withHandle(db ->
-                db.createQuery("SELECT \"$ObjectName\" FROM \"jmx.java.lang:type=Runtime\"").mapTo(String.class).first());
+                db.createQuery("SELECT \"$ObjectName\" FROM \"jmx.java.lang:type=Runtime\"").mapTo(String.class).findOnly());
         assertEquals("java.lang:type=Runtime", objectName);
     }
 
@@ -79,4 +85,52 @@ public class ManageQLServerTest {
                 db.createQuery("SELECT \"$ObjectName\" FROM \"jmx.java.lang:*\"").mapTo(String.class).list()));
         assertTrue(objectNames.contains("java.lang:type=Runtime"));
     }
+
+    @Test
+    public void canSelectArrayAttribute() throws SQLException, InterruptedException {
+        String[] value = dbi.withHandle(db ->
+                db.createQuery("SELECT MemoryPoolNames FROM \"jmx.java.lang:type=GarbageCollector,name=PS MarkSweep\"")
+                        .mapTo(String[].class)
+                        .findOnly());
+        // System.out.println(Arrays.asList(value));
+        assertEquals("PS Eden Space", value[0]);
+    }
+
+    @Test
+    public void canSelectMapAttribute() throws SQLException, InterruptedException {
+
+        String value = dbi.withHandle(db ->
+                db.createQuery("SELECT LastGcInfo FROM \"jmx.java.lang:type=GarbageCollector,name=PS MarkSweep\"")
+                        .mapTo(String.class)
+                        .findOnly());
+
+        Gson gson = new Gson();
+        Map<String, Object> data = gson.fromJson(value, HashMap.class);
+        Map<String, Object> memoryUsageBeforeGc = (Map<String, Object>)((List<Object>) data.get("memoryUsageBeforeGc")).get(0);
+
+        System.out.println(memoryUsageBeforeGc);
+        assertTrue(memoryUsageBeforeGc.containsKey("key"));
+        assertTrue(memoryUsageBeforeGc.containsKey("value"));
+
+    }
+
+    @Test
+    public void canSelectComplexAttribute() throws SQLException, InterruptedException {
+
+        String value = dbi.withHandle(db ->
+                db.createQuery("SELECT DiagnosticOptions FROM \"jmx.com.sun.management:type=HotSpotDiagnostic\"")
+                        .mapTo(String.class)
+                        .findOnly());
+
+        Gson gson = new Gson();
+        Map<String, Object> data = (Map<String, Object>) gson.fromJson(value, List.class).get(0);
+        assertTrue(data.containsKey("name"));
+        assertTrue(data.containsKey("origin"));
+        assertTrue(data.containsKey("value"));
+        assertTrue(data.containsKey("writeable"));
+    }
+
+
+
+
 }
